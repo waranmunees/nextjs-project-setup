f57ceba6c33611db4159f4724c659d4ccf9f059e
Muneeswaran <130306504+waranmunees@users.noreply.github.com>
2025-07-25 09:40:53 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753416652272

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a8853cff._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_date-fns_5e7b5adc._.js",
+  "static/chunks/node_modules_react-day-picker_dist_index_esm_9fc30424.js",
+  "static/chunks/node_modules_eca4a5c1._.js"
+],
+    source: "dynamic"
+});
